5db54c63b58725eef46d4053910ea87fa2ce846f
Pedro Paulo Amorim <pepa.amorim@gmail.com>
2015-12-19 15:45:18 -0200

    base structure

---
+++ Example/Pods/Target Support Files/UpdateAnimation2/UpdateAnimation2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UpdateAnimation2 : NSObject
+@end
+@implementation PodsDummy_UpdateAnimation2
+@end
---
+++ Example/Pods/Target Support Files/UpdateAnimation2/UpdateAnimation2-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double UpdateAnimation2VersionNumber;
+FOUNDATION_EXPORT const unsigned char UpdateAnimation2VersionString[];
+
